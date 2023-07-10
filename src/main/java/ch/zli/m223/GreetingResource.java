5c6c6f3f3b9488f9ad28efa8ff3be34d02dfb80d
francescoryu <francescoryu04@gmail.com>
2023-07-10 11:29:23 +0000

    calc method working

--- src/main/java/ch/zli/m223/GreetingResource.java
+++ src/main/java/ch/zli/m223/GreetingResource.java
@@ -2,15 +2,16 @@ package ch.zli.m223;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/calc")
 public class GreetingResource {
-
     @GET
+    @Path("/{x}/{y}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public int calc(@PathParam("x") int x, @PathParam("y") int y) {
+        return x + y;
     }
 }
